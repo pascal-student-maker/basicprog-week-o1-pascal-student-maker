a9a2306c475b0490d162f4dd1ff131c401074a9d
pascal-student-maker <pascal.musabyimana@student.howest.be>
2025-01-11 12:37:36 +0100

    Update exercise 12.py

--- exercise 12.py
+++ exercise 12.py
@@ -1,9 +1,14 @@
 #exercise 12
 from math import sqrt
-a = int(input("Enter the length of the side a:"))
-b = int(input("Enter the length of the side b:"))
-c = int(input("Enter the length of the side c:"))
-s = (a+b+c)*0.5
-heron_formula = sqrt(s*(s-a) * (s-b) * (s-c))
-print(f" the surface area is : %.2f" % heron_formula)
+a = int(input("Enter the length of side a:"))
+b = int(input("Enter the length of side b:"))
+c = int(input("Enter the length of side c:"))
+
+s = (a +b+c)*0.5
+
+heron_formula = sqrt(s*(s-a) *(s-b) *(s-c))
+
+print(f" The  surface   area  is:  %.2f" % heron_formula ,"cm²")
+
+
 
